f87d400b5f9cac7164364a12ab88ba7b355caf4c
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-21 12:28:13 +0100

    build: add git hooks generation

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,11 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15"
 }
 
 rootProject.name = "spe-helloworld-plugin"
+
+gitHooks {
+    commitMsg { conventionalCommits() }
+    createHooks(overwriteExisting = true)
+}
