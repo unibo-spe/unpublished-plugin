ca5476b2a86f86178cce43d4216556f125bc3b84
Danilo Pianini <danilo.pianini@gmail.com>
2024-11-21 10:50:26 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("com.gradle.develocity") version "3.18.2"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl = "https://gradle.com/terms-of-service"
+        termsOfUseAgree = "yes"
+        uploadInBackground = !System.getenv("CI").toBoolean()
+    }
+}
+
+gitHooks {
+    preCommit {
+        tasks("ktlintCheck")
+    }
+    commitMsg { conventionalCommits() }
+    createHooks(true)
+}
+
+rootProject.name = "template-for-gradle-plugins"
