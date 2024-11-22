db819e3b0c9f0c5849a03b11b697ecba90b4dd13
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-22 12:46:09 +0100

    build: configure develocity/build scans

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.15"
+    id("com.gradle.develocity") version "3.18.2"
 }
 
 rootProject.name = "spe-helloworld-plugin"
@@ -11,4 +12,11 @@ gitHooks {
     }
     commitMsg { conventionalCommits() }
     createHooks(overwriteExisting = true)
+}
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
 }
